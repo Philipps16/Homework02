ce3bdeed1b99472fe3d46d91000f25052cac2ced
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-06 17:39:37 +0300

    Выполнил упражнение 01

---
+++ Example_01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear ();
+
+Console.WriteLine ("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = (a / 10) % 10;
+if (a < 100 && a > 999)
+    Console.WriteLine("!!! Введите ТРЕХЗНАЧНОЕ число !!!");
+else
+{
+    Console.WriteLine ($"Вторая цифра числа: {b}");
+}
