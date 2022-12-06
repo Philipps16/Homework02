17be48bc2521858818d2f47c574f0f89acff77d6
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-06 17:50:57 +0300

    Выполнил упражнение 3

---
+++ Example_03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите номер дня недели: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write ("Вы ошиблись! Введите новое число: ");
+n = Convert.ToInt32(Console.ReadLine());
+};
+if (n < 6)
+{
+    Console.WriteLine("Это рабочий день");
+}    
+else
+    Console.WriteLine("Это выходной!!!!!");
