1148f68c11a56cecf6b99067246c2f538639067a
Philipp Mitrofanov <press.fm@mail.ru>
2022-12-06 18:08:06 +0300

    Выполнил упражнение 2

---
+++ Example_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int b = 0;
+if (n < 100)
+    Console.WriteLine ("Третьей цифры нет");
+else
+{
+while (n > 999)
+    n = n / 10;
+    b = n % 10;     
+
+Console.WriteLine ($"Третья цифра: {b}");
+}
